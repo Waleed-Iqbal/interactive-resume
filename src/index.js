9304c9547ac9e7eabc7f12eda6612579a8a8f5ad
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2019-01-13 15:00:06 +0500

    cleared up the code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.css";
-import Game from "./components/Game";
 
 ReactDOM.render(<Game />, document.getElementById("root"));
