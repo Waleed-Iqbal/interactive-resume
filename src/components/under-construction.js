d39582ee246493cf581d34b16f804996969dc545
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-04-30 22:05:03 +0500

    Started practicing SVGs

--- src/components/under-construction.js
+++ src/components/under-construction.js
@@ -1,23 +1,24 @@
 import React from "react";
 import "../styles/under-construction.scss";
 import { AvatarWaleed } from "./avatars/waleed/avatar-waleed";
+import svgComingSoon from "../assets/coming-soon.svg";
 
 export function UnderConstruction() {
   return (
     <div id="UnderConstruction">
-      <div id="HomePage">
-        <div className="div--bottom__center">
-          Under Construction
-        </div>
+      {/* <div id="HomePage">
+        <div className="div--bottom__center">Under Construction</div>
       </div>
       <div id="Coming">
-        <div className="div--middle__center"></div>
+        <div className="div--middle__center" />
       </div>
       <div id="Soon">
         <div>Coming S</div>
         <AvatarWaleed />
         <div>N</div>
-      </div>
+      </div> */}
+
+      <img src={svgComingSoon} alt="coming-soon" />
     </div>
   );
 }
