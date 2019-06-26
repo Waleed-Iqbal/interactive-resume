0e302fca5ff389da19f547fc9ca0e2d8d662441d
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-06-27 02:37:47 +0500

    add skeleton structure

--- src/components/resume.js
+++ src/components/resume.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import "../styles/resume.scss";
 
 
-class Resume extends React.Component {
-  render() {
-    return (
-      <div className="resume-container">
-        This is my resume
-      </div>
-    );
-  }
+export function Resume() {
+  return (
+    <main className="resume-container">
+      <section className="name designation"></section>
+      <section className="somethings-about-me"></section>
+      <section className="content">
+        <div className="contact-info">
+          <div className="contact-me"></div>
+          <div className="hire-me"></div>
+          <div className="social-presence"></div>
+        </div>
+        <div className="skill-set"></div>
+        <div className="work-experience"></div>
+        <div className="certifications"></div>
+        <div className="education"></div>
+        <div className="interests"></div>
+      </section>
+      This is my resume
+    </main>
+  );
 }
 
 export default Resume;
