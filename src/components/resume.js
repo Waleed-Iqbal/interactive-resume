bd88fc574629fa943b547d7675a52efdd5f5a75b
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-06-27 03:05:54 +0500

    add online profile links

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import "../styles/resume.scss";
 
 
@@ -7,7 +8,7 @@ export function Resume() {
     <main className="resume-container">
       <section className="name designation">
         <div className="first-name">Khawaja</div>
-        <title>Frontend Developer</title>
+        <title>Frontend Developer, Web Animator, Brings your online presense to life</title>
         <div className="last-name">Waleed Iqbal</div>
       </section>
       <section className="somethings-about-me"></section>
@@ -15,20 +16,41 @@ export function Resume() {
         <div className="contact-info">
           <div className="contact-me">
             <ul>
-              <li className="email"></li>
-              <li className="phone-number"></li>
-              <li className="linked-in"></li>
+              <li className="email">ikhawajawaleed@gmail.com</li>
+              <li className="linked-in">
+                <a href="https://www.linkedin.com/in/kwaleediqbal" target="_black">
+                  LinkedIn profile
+                </a>
+              </li>
             </ul>
           </div>
           <div className="hire-me">
-            <p className="fiverr"></p>
+            <p className="fiverr">
+              <a href="https://www.fiverr.com/waleed_iqbal?up_rollout=true" target="_black">
+                Fiverr 5 Star Rated Profile
+              </a>
+            </p>
           </div>
           <div className="social-presence">
             <ul>
-              <li className="git-hub"></li>
-              <li className="stack-overflow"></li>
-              <li className="facebook"></li>
-              <li className="quora"></li>
+              <li className="git-hub">
+                <a href="https://github.com/Waleed-Iqbal" target="_black">Github</a>
+              </li>
+              <li className="stack-overflow">
+                <a href="https://stackoverflow.com/users/4758651/waleed-iqbal" target="_black">
+                  Stackoverflow
+                </a>
+              </li>
+              <li className="facebook">
+                <a href="https://web.facebook.com/KhawajaWaleedIqbal" target="_black">
+                  Facebook
+                </a>
+              </li>
+              <li className="quora">
+                <a href="https://www.quora.com/profile/Khawaja-Waleed-Iqbal" target="_black">
+                  Quora
+                </a>
+              </li>
             </ul>
           </div>
         </div>
