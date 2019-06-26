52721feb1d6356faaa0dd99763bc742509d4adb8
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-06-27 01:48:53 +0500

    resume navigation placeholder added

--- src/components/under-construction.js
+++ src/components/under-construction.js
@@ -1,10 +1,14 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 import "../styles/under-construction.scss";
 import { AvatarWaleed } from "./avatars/waleed/avatar-waleed";
 
 export function UnderConstruction() {
   return (
     <div id="UnderConstruction">
+      <Link href="/resume">
+        <h1>RESUME HERE !!!</h1>
+      </Link>
       <div id="HomePage">
         <div className="div--bottom__center">
           Under Construction
