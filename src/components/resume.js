b0468b7ea2bb199933524214e2015cc5dfc91783
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-06-27 02:42:47 +0500

    add some content structure

--- src/components/resume.js
+++ src/components/resume.js
@@ -5,13 +5,32 @@ import "../styles/resume.scss";
 export function Resume() {
   return (
     <main className="resume-container">
-      <section className="name designation"></section>
+      <section className="name designation">
+        <div className="first-name">Khawaja</div>
+        <title>Frontend Developer</title>
+        <div className="last-name">Waleed Iqbal</div>
+      </section>
       <section className="somethings-about-me"></section>
       <section className="content">
         <div className="contact-info">
-          <div className="contact-me"></div>
-          <div className="hire-me"></div>
-          <div className="social-presence"></div>
+          <div className="contact-me">
+            <ul>
+              <li className="email"></li>
+              <li className="phone-number"></li>
+              <li className="linked-in"></li>
+            </ul>
+          </div>
+          <div className="hire-me">
+            <p className="fiverr"></p>
+          </div>
+          <div className="social-presence">
+            <ul>
+              <li className="git-hub"></li>
+              <li className="stack-overflow"></li>
+              <li className="facebook"></li>
+              <li className="quora"></li>
+            </ul>
+          </div>
         </div>
         <div className="skill-set"></div>
         <div className="work-experience"></div>
@@ -19,7 +38,6 @@ export function Resume() {
         <div className="education"></div>
         <div className="interests"></div>
       </section>
-      This is my resume
     </main>
   );
 }
