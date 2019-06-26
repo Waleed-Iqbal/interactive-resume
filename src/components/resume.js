ef98d23de7677a95dbef70587b553e43f7cd8b1a
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-06-27 03:19:39 +0500

    add some taglines

--- src/components/resume.js
+++ src/components/resume.js
@@ -16,7 +16,7 @@ export function Resume() {
         <div className="contact-info">
           <div className="contact-me">
             <ul>
-              <li className="email">ikhawajawaleed@gmail.com</li>
+              <li className="email">ikhawajawaleed@gmail.com Don't be shy. Drop me an email</li>
               <li className="linked-in">
                 <a href="https://www.linkedin.com/in/kwaleediqbal" target="_black">
                   LinkedIn profile
@@ -29,27 +29,34 @@ export function Resume() {
               <a href="https://www.fiverr.com/waleed_iqbal?up_rollout=true" target="_black">
                 Fiverr 5 Star Rated Profile
               </a>
+              <p>Have a peek</p>
             </p>
           </div>
           <div className="social-presence">
             <ul>
               <li className="git-hub">
-                <a href="https://github.com/Waleed-Iqbal" target="_black">Github</a>
+                <a href="https://github.com/Waleed-Iqbal" target="_black">
+                  Github
+                </a>
+                <p className="">Wanna copy paste code?</p>
               </li>
               <li className="stack-overflow">
                 <a href="https://stackoverflow.com/users/4758651/waleed-iqbal" target="_black">
                   Stackoverflow
                 </a>
+                <p className="">Want some stylish answers?</p> 
               </li>
               <li className="facebook">
                 <a href="https://web.facebook.com/KhawajaWaleedIqbal" target="_black">
                   Facebook
                 </a>
+                <p className=""> Wanna stalk?</p>
               </li>
               <li className="quora">
                 <a href="https://www.quora.com/profile/Khawaja-Waleed-Iqbal" target="_black">
                   Quora
                 </a>
+                <p className="">Lets ask questions together</p>
               </li>
             </ul>
           </div>
