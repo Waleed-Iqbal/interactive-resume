06f277c8bd081ff41decbab068f7e3a6ad7806e7
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-06-29 00:39:23 +0500

    add custom title to each routed component (page)

--- src/components/app.js
+++ src/components/app.js
@@ -7,35 +7,49 @@ import Resume from "./resume";
 import { Error404 } from './error404';
 
 class App extends React.Component {
-  //constructor(props) {
-  //  super(props);
-  //}
 
   render() {
     return (
       <React.Fragment>
         {/* NavBar is not inside Switch since its gonna be displayed on all pages */}
         <Switch>
-          <Route exact path="/" component={UnderConstruction} />
           <Route 
             exact 
-            path="/resume" 
-            render = {props => (
-              <Page {...props} component={Resume} title="Resume" />
-            )} />
-          <Route component={Error404} />
+            path="/" 
+            render={
+              props => (
+                <Page {...props} component={UnderConstruction} title="Coming Soon" />
+              )
+            }
+          />
+          <Route
+            exact
+            path="/resume"
+            render={
+              props => (
+                <Page {...props} component={Resume} title="Resume" />
+              )
+            }
+          />
+          <Route
+            render={
+              props => (
+                <Page {...props} component={Error404} title="Page not found" />
+              )
+            }
+          />
         </Switch>
       </React.Fragment>
     );
   }
 }
 
-
+// The following component is used to add custom title while routing
 class Page extends React.Component {
   componentDidMount() {
     document.title = this.props.title
   }
-  
+
   render() {
     const PageComponent = this.props.component
 
