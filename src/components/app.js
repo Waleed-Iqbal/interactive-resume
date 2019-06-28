f3dd1a3d875c2407e1088c88403ddc9688dae49d
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-06-29 03:35:38 +0500

    add a demo's component

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import "../styles/globals.scss";
 import "../styles/under-construction.scss";
 import { UnderConstruction } from "./under-construction";
+import { Demos } from "../components/demos/demos";
 import Resume from "./resume";
 import { Error404 } from './error404';
 
@@ -13,9 +14,9 @@ class App extends React.Component {
       <React.Fragment>
         {/* NavBar is not inside Switch since its gonna be displayed on all pages */}
         <Switch>
-          <Route 
-            exact 
-            path="/" 
+          <Route
+            exact
+            path="/"
             render={
               props => (
                 <Page {...props} component={UnderConstruction} title="Coming Soon" />
@@ -28,6 +29,15 @@ class App extends React.Component {
             render={
               props => (
                 <Page {...props} component={Resume} title="Resume" />
+              )
+            }
+          />
+          <Route
+            exact
+            path="/demos"
+            render={
+              props => (
+                <Page {...props} component={Demos} title="Demo & Samples" />
               )
             }
           />
@@ -48,7 +58,7 @@ class App extends React.Component {
  * Add a component as page when routed with custom tab
  */
 class Page extends React.Component {
-  
+
   componentDidMount() {
     document.title = this.props.title
   }
