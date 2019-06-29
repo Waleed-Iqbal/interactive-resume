9836aa2434fb41119681f561e481d18d96524bab
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-06-29 16:21:43 +0500

    start home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import "../styles/home.scss";
 
 export default function Home() {
   return (
-    <div>
-      This is home page
+    <div class="home-container">
+      <nav className="main-left-nav">
+        <ul className="nav-tabs">
+          <li className="nav-home">
+            <Link to="/home" >
+              Home
+            </Link>
+          </li>
+          <li className="nav-simplistic-resume">
+            Simplistic Resume
+          </li>
+          <li className="nav-interactive-resume">
+            Immersive Resume
+          </li>
+          <li className="nav-demos-samples">
+            Demos &amp; Samples
+          </li>
+          <li className="nav-blog">
+            Blog
+          </li>
+          <li className="nav-contact">
+            Get in touch
+          </li>
+        </ul>
+      </nav>
+      <main className="main-section"></main>
     </div>
   )
 }
