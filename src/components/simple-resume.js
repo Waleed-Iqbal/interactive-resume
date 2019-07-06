edb19b6c15708857d78a1a9c6e89e0c3f82cc776
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-07-07 02:09:06 +0500

    update resume structure

--- src/components/simple-resume.js
+++ src/components/simple-resume.js
@@ -12,15 +12,14 @@ export function SimpleResume() {
           <h1 className="name">Waleed Iqbal</h1>
           <h1 className="designation">Frontend Developer</h1>
         </div>
+      <div className="somethings-about-me">
+        <h3>I excel at:</h3>
+          turning complex into simple
+          turning lag into optimization
+          turning designs into websites/webapps
+      </div>
       </section>
-      <section className="somethings-about-me">
-        I turn complex into simple
-        I turn chaos into order
-        I turn coffee into code
-        I turn designs into websites/webapps
-      </section>
-      <section className="content">
-        <div className="contact-info">
+        <section className="contact-info">
           <div className="contact-me">
             <ul>
               <li className="email">ikhawajawaleed@gmail.com Don't be shy. Drop me an email</li>
@@ -67,16 +66,16 @@ export function SimpleResume() {
               </li>
             </ul>
           </div>
-        </div>
-        <div className="skill-set"></div>
-        <div className="work-experience">
+        </section>
+        <section className="skill-set"></section>
+        <section className="work-experience">
           <li>KeepTrucking</li>
           <li>NowSoftware Inc.</li>
           <li>Bentley Systems Inc.</li>
           <li>Contour Software Inc. / Trapeze</li>
           <li>LMKR</li>
-        </div>
-        <div className="certs-honors">
+        </section>
+        <section className="certs-honors">
           <ul>
             <li>Microsoft Certified Professional. (Oct 2015)</li>
             <li>Gold Medal for best undergraduate final year project awarded by National University of Science and Technology. (Aug 2014)</li>
@@ -84,20 +83,19 @@ export function SimpleResume() {
             <li>Merit certificate from Rector NUST for being among the high achievers of the institute. (Mar 2014)</li>
             <li>Gold Medal and Cash Prize from school in Matriculation exams. (2008)</li>
           </ul>
-        </div>
-        <div className="education">
+        </section>
+        <section className="education">
           <li>Undergraduate</li>
           <li>College</li>
           <li>School</li>
-        </div>
-        <div className="interests">
+        </section>
+        <section className="interests">
           <ul>
             <li>Cycling</li>
             <li>Reading</li>
             <li>Playing and making games</li>
           </ul>
-        </div>
-      </section>
+        </section>
     </main>
   );
 }
