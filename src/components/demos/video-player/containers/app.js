b63b4a820f533624a7bbf2d3e7dbd9b177363870
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-08-04 11:40:23 +0500

    add video player scafolding

---
+++ src/components/demos/video-player/containers/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const VideoPlayer = () => (
+  <div>music player</div>
+)
+
+export default VideoPlayer;
---
+++ src/components/demos/video-player/containers/video-player.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Video from '../video';
+import Playlist from './playlist';
+
+export default function VideoPlayer() {
+  return (
+    <div>
+      <Video />
+      <Playlist />
+    </div>
+  );
+}
