5a6359d389bc685860f528929809c603cee1b96b
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-08-05 17:36:23 +0500

    add theme styles

--- src/components/demos/video-player/containers/video-player.js
+++ src/components/demos/video-player/containers/video-player.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import Video from '../video';
 import Playlist from './playlist';
+import { ThemeProvider } from 'styled-components';
+import StyledVideoPlayer from '../styles/styled-video-player';
+
+const theme = {
+  border: "#353535",
+  bgcolorItem: "#414141",
+  bgcolorItemArchive: "#405c63",
+  bgcolorPlayed: "#526d4e",
+  border: "none",
+  borderPlayed: "none",
+  color: "#fff"
+}
+
+const themeLight = {
+  border: "#fff",
+  bgcolorItem: "#fff",
+  bgcolorItemArchive: "#80a7b1",
+  bgcolorPlayed: "#7d9979",
+  border: "1px solid #353535",
+  borderPlayed: "none",
+  color: "##353535"
+}
 
 const VideoPlayer = (props) => {
   return (
-    <React.Fragment>
-      <Video />
-      <Playlist />
-    </React.Fragment>
+    <ThemeProvider theme={state.nightMode ? theme: themeLight }>
+      <StyledVideoPlayer>
+        <Video />
+        <Playlist />
+      </StyledVideoPlayer>
+    </ThemeProvider>
   );
 }
 
