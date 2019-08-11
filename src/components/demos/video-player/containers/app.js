90e1080f1b078041ec438568a9c9ae2afa2794ed
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-08-11 18:54:05 +0500

    add referece

--- src/components/demos/video-player/containers/app.js
+++ src/components/demos/video-player/containers/app.js
@@ -8,6 +8,9 @@ const VideoPlayerApp = ({props}) => {
     <React.Fragment>
       <GlobalStyle />
       <VideoPlayer props={props} />
+      <h2 className="text-center mt-40">
+        <a href="https://www.youtube.com/watch?v=iVRO0toVdYM">Developed from a project by FreeCodeCamp</a>.
+      </h2>
     </React.Fragment>
   );
 }
