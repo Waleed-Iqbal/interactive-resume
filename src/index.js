716d9e3c1df0fa9be73ad57e96bb80706738043a
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-08-11 14:13:40 +0500

    update formatting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, BrowserRouter } from "react-router-dom";
 import App from "./components/app";
 
 ReactDOM.render(
-    <HashRouter>
-      <App />
-    </HashRouter>,
-document.getElementById("root"));
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById("root"));
