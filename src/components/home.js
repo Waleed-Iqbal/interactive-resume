76b9c11138b5e4f1d206d51d73484101441df772
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-08-14 03:06:07 +0500

    navigation moved to separate component

--- src/components/home.js
+++ src/components/home.js
@@ -1,37 +1,9 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+
 import "../styles/home.scss";
-import "../styles/navigation-pane.scss";
 
 export default function Home() {
   return (
-    <div className="home-container">
-      <nav className="main-left-nav">
-        <ul className="nav-tabs">
-          <NavigationItem navigationClass="nav-home" linkTo="/home" text="Home" />
-          <NavigationItem navigationClass="nav-about" linkTo="/aboutwaleed" text="About me" />
-          <NavigationItem navigationClass="nav-simplistic-resume" linkTo="/simple-resume" text="Resume" />
-          {/* <NavigationItem navigationClass="nav-interactive-resume" linkTo="/interactive-resume" text="Immersive Resume" /> */}
-          {/* <NavigationItem navigationClass="nav-demos-samples" linkTo="/demos" text="Demos" /> */}
-          <NavigationItem navigationClass="nav-blog" linkTo="/blog" text="Blog" />
-          <NavigationItem navigationClass="nav-contact" linkTo="/contact-me" text="Contact Me" />
-          <li className="demos"><p>Demos</p></li>
-          <NavigationItem navigationClass="nav-video-player" linkTo="/video-player" text="Video Player" />
-        </ul>
-      </nav>
-      <main className="main-section"></main>
-    </div>
+    <main className="main-section"></main>
   )
-}
-
-class NavigationItem extends Component {
-  render() {
-    return (
-      <li className={this.props.navigationClass}>
-          <Link to={this.props.linkTo}>
-            <p>{this.props.text}</p>
-          </Link>
-      </li>
-    )
-  }
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter, BrowserRouter } from "react-router-dom";
 import App from "./components/app";
+import "./styles/home.scss";
+import NavigationPane from './components/navigation-pane';
 
 ReactDOM.render(
   <HashRouter>
-    <App />
+    <div className="home-container">
+      <NavigationPane />
+      <App />
+    </div>
   </HashRouter>,
   document.getElementById("root"));
