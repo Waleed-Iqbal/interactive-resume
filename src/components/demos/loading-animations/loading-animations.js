77199d2bd12128449e9532ec19bb9dbeb72b1937
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-08-15 03:06:19 +0500

    Add loader component for simple loaders

--- src/components/demos/loading-animations/loading-animations.js
+++ src/components/demos/loading-animations/loading-animations.js
@@ -4,33 +4,34 @@ import './loading-animations.scss';
 export default function LoadingAnimations() {
   return (
     <div id="loaders-container">
-      <div>
-        <div className="loader loader-1"></div>
-        <p>
-          Loader 1
-        </p>
-      </div>
-      <div>
-        <div className="loader loader-2"></div>
-        <p>Loader 2</p>
-      </div>
-      <div>
+      <SimpleLoader loaderClass="loader-1" />
+      <SimpleLoader loaderClass="loader-2" />
+      <div className="loader-container">
         <div className="loader loader-3">
           <div className="loader-3-inner"></div>
         </div>
-        <p>Loader 3</p>
+        <p className="loader-name">Loader 3</p>
       </div>
-      <div>
-        <div className="loader loader-4"></div>
-        <p>Loader 4</p>
-      </div>
-      <div>
-        <div className="loader loader-5"></div>
-        <p>Loader 5</p>
-      </div>
+      <SimpleLoader loaderClass="loader-4" />
+      <SimpleLoader loaderClass="loader-5" />
       {/* <div>
         <img className="company-loader" width="350px" height="130px" src="https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo1.png" />
       </div> */}
+    </div>
+  )
+}
+
+// Simple loader means the one with no nesting inside the div with loader-x class
+function SimpleLoader({ loaderClass }) {
+
+  // Converting "loader-1" to "Loader 1"
+  let loaderName = loaderClass.split('-').join(' ').toString();
+  loaderName = loaderName[0].toUpperCase() + loaderName.slice(1);
+
+  return (
+    <div className="loader-container">
+      <div className={`loader ${loaderClass}`}></div>
+      <p className="loader-name">{loaderName}</p>
     </div>
   )
 }
