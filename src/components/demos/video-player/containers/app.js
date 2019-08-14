e1e4c55a209c59313f07e99f218770e145e01e57
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-08-15 01:18:02 +0500

    update code formatting

--- src/components/demos/video-player/containers/app.js
+++ src/components/demos/video-player/containers/app.js
@@ -2,14 +2,14 @@ import React from 'react';
 import VideoPlayer from './video-player';
 import GlobalStyle from '../styles/global-style';
 
-const VideoPlayerApp = ({props}) => (
-    <div className="main-section">
-      <GlobalStyle />
-      <VideoPlayer props={props} />
-      <h2 className="text-center mt-40">
-        <a href="https://www.youtube.com/watch?v=iVRO0toVdYM">Developed from a project by FreeCodeCamp</a>.
-      </h2>
-    </div>
-    );
+const VideoPlayerApp = ({ props }) => (
+  <React.Fragment>
+    <GlobalStyle />
+    <VideoPlayer props={props} />
+    <h2 className="text-center mt-40">
+      <a href="https://www.youtube.com/watch?v=iVRO0toVdYM">Developed from a project by FreeCodeCamp</a>.
+    </h2>
+  </React.Fragment>
+);
 
 export default VideoPlayerApp;
