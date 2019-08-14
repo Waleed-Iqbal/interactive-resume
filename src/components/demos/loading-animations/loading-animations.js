8437e9201bc3e9c2b72a84b23c337ea857132d25
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-08-15 02:36:19 +0500

    Text improved

--- src/components/demos/loading-animations/loading-animations.js
+++ src/components/demos/loading-animations/loading-animations.js
@@ -5,28 +5,28 @@ export default function LoadingAnimations() {
   return (
     <div id="loaders-container">
       <div>
-        <div className="loader-1"></div>
+        <div className="loader loader-1"></div>
         <p>
-          loader-1
+          Loader 1
         </p>
       </div>
       <div>
-        <div className="loader-2"></div>
-        <p>loader-2</p>
+        <div className="loader loader-2"></div>
+        <p>Loader 2</p>
       </div>
       <div>
-        <div className="loader-3">
+        <div className="loader loader-3">
           <div className="loader-3-inner"></div>
         </div>
-        <p>loader-3</p>
+        <p>Loader 3</p>
       </div>
       <div>
-        <div className="loader-4"></div>
-        <p>loader-4</p>
+        <div className="loader loader-4"></div>
+        <p>Loader 4</p>
       </div>
       <div>
-        <div className="loader-5"></div>
-        <p>loader-5</p>
+        <div className="loader loader-5"></div>
+        <p>Loader 5</p>
       </div>
       {/* <div>
         <img className="company-loader" width="350px" height="130px" src="https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo1.png" />
