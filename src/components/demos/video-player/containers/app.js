e2426c9fb12f723caf2a3d575621d72c91f95a58
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-08-14 15:23:49 +0500

    need to avoid adding main-section every time

--- src/components/demos/video-player/containers/app.js
+++ src/components/demos/video-player/containers/app.js
@@ -3,13 +3,13 @@ import VideoPlayer from './video-player';
 import GlobalStyle from '../styles/global-style';
 
 const VideoPlayerApp = ({props}) => (
-    <React.Fragment>
+    <div className="main-section">
       <GlobalStyle />
       <VideoPlayer props={props} />
       <h2 className="text-center mt-40">
         <a href="https://www.youtube.com/watch?v=iVRO0toVdYM">Developed from a project by FreeCodeCamp</a>.
       </h2>
-    </React.Fragment>
-  );
+    </div>
+    );
 
 export default VideoPlayerApp;
