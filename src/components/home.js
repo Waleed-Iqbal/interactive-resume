533bc79f3316b380f759edcafe069e528a7b2b61
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-08-18 22:24:23 +0500

    complete slider profile pic

--- src/components/home.js
+++ src/components/home.js
@@ -5,16 +5,28 @@ import "../styles/home.scss";
 export default function Home() {
 
   let profilePictureData = {};
+  let avatar = null;
+  let start = null;
+  let newPositionOfAvatar = 0;
+
   useEffect(() => {
     profilePictureData = document.getElementsByClassName('profile-picture-slider')[0].getClientRects()[0];
+    avatar = document.getElementsByClassName('profile-picture-avatar')[0];
     console.log(profilePictureData);
   }, []);
 
+  function slideAvatar(timestamp) {
+    if (!start) start = timestamp;
+    var progress = timestamp - start;
+    avatar.style.left = newPositionOfAvatar;
+    if (progress < 400) {
+      window.requestAnimationFrame(slideAvatar);
+    }
+  }
+
   let sliderEffectHandler = (e) => {
-    console.log(profilePictureData.left);
-    console.log(e.pageX);
-    console.log(e.pageY);
-    console.log("----------------------------");
+    newPositionOfAvatar = (e.pageX - profilePictureData.left) + "px";
+    window.requestAnimationFrame(slideAvatar);
   }
 
   return (
@@ -22,9 +34,9 @@ export default function Home() {
       <h1>Hi, Ni Hau, Hola, Bonjure, Salam, Marhaba, Guten Tag, Namaste, Konnichiwa </h1>
       {/* <img src="https://i.imgur.com/ID1ZAgw.jpg" alt="profile picture"/> */}
       {/* <img src={require('../images/profile-pic.jpg')} alt="profile picture"/> */}
-      <div className="profile-picture-slider" onMouseEnter={sliderEffectHandler} >
+      <div className="profile-picture-slider" onMouseEnter={sliderEffectHandler} onMouseMove={sliderEffectHandler} onMouseLeave={sliderEffectHandler} >
         <img className="profile-picture" src={require('../images/profile-pic.jpg')} alt="profile picture" />
-        <img className="profile-picture-avatar" src={require('../images/profile-pic-avatar.png')} alt="profile picture avatar"/>
+        <img className="profile-picture-avatar" src={require('../images/profile-pic-avatar.jpg')} alt="profile picture avatar" />
       </div>
     </div>
   )
