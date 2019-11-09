86dca926c661800478ba0a82faf9e2e3be22c566
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-11-09 19:01:39 +0500

    change a file name

---
+++ src/scripts/pages-data.js
@@ -0,0 +1,63 @@
+
+const AppNavigationLinks = {
+  default: '/',
+  home: '/home',
+  blog: 'blog',
+  morseCode: '/morseCode',
+  contactMe: '/contact-me',
+  aboutWaleed: '/about-waleed',
+  simpleResume: '/simple-resume',
+  interactiveResume: '/interactive-resume',
+  loadingAnimations: '/loading-animations',
+
+  videoPlayer: '/video-player',
+  get videoPlayer_ActiveVideo() {
+    return `${this.AppNavigationLinks.videoPlayer}:activeVideo`;
+  }
+}
+
+const HomePage = {
+  pageTitle: 'Hire Waleed',
+  greetings: ['Hi', 'Ni Hau', 'Hola', 'Bonjour', 'Salam', 'Marhaba', 'Guten Tag', 'Namaste', 'Konnichiwa'],
+}
+
+const AboutPage = {
+  pageTitle: 'About Waleed',
+}
+
+const SimpleResume = {
+  pageTitle: 'Resume',
+
+}
+
+const LoadingAnimationsPage = {
+  pageTitle: 'Animating Loaders',
+}
+
+const VideoPlayerPage = {
+  pageTitle: 'Video Player',
+}
+
+const PageNotFound = {
+  pageTitle: 'Page Not Found'
+}
+
+const PageTitles = {
+  homePage: HomePage.pageTitle,
+  aboutPage: AboutPage.pageTitle,
+  pageNotFound: PageNotFound.pageTitle,
+  simpleResume: SimpleResume.pageTitle,
+  videoPlayer: VideoPlayerPage.pageTitle,
+  loadingAnimationsPage: LoadingAnimationsPage.pageTitle,
+}
+
+export {
+  HomePage,
+  PageTitles,
+  AboutPage,
+  PageNotFound,
+  SimpleResume,
+  VideoPlayerPage,
+  AppNavigationLinks,
+  LoadingAnimationsPage,
+} ;
