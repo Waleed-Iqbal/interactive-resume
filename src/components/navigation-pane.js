8fe4d1c07f555010065d2ba498562b88b7306eb1
waleed-iqbal <ikhawajawaleed@gmail.com>
2020-06-13 16:27:08 +0500

    test change

--- src/components/navigation-pane.js
+++ src/components/navigation-pane.js
@@ -7,7 +7,7 @@ export default function NavigationPane() {
   return (
     <nav className="main-left-nav">
       <ul className="nav-tabs">
-        <NavigationItem navigationClass="nav-home" linkTo={AppNavigationLinks.home} text="Home" />
+        <NavigationItem navigationClass="nav-home" linkTo={AppNavigationLinks.home} text="Home Amar" />
         {/* <NavigationItem navigationClass="nav-about" linkTo="/aboutwaleed" text="About me" /> */}
         <NavigationItem navigationClass="nav-simplistic-resume" linkTo={AppNavigationLinks.simpleResume} text="Resume" />
         {/* <NavigationItem navigationClass="nav-interactive-resume" linkTo="/interactive-resume" text="Immersive Resume" /> */}
