c5de6e3a466ec0ffb83a7c1ac2d21f5eb100f980
waleed-iqbal <ikhawajawaleed@gmail.com>
2021-01-17 12:52:49 +0500

    update hamburger menu animation

--- src/components/navigation-pane.js
+++ src/components/navigation-pane.js
@@ -13,52 +13,50 @@ export default function NavigationPane() {
 
   return (
     <header className='header horz-padding'>
-      <nav className='header-nav'>
-        <div className='hamburger' onClick={hamburgerClicked}>
-          {/* add class "W" to change the state */}
-          <div className='line line-1'></div>
-          <div className='line line-2'></div>
-          <div className='line line-3'></div>
-          <div className='line line-4'></div>
-        </div>
-        <div className='nav-tabs-container'>
-          <ul className='nav-tabs'>
-            <NavigationItem
-              navigationClass='nav-home'
-              linkTo={AppNavigationLinks.home}
-              text='Home'
-            />
-            {/* <NavigationItem navigationClass="nav-about" linkTo="/aboutwaleed" text="About me" /> */}
-            <NavigationItem
-              navigationClass='nav-simplistic-resume'
-              linkTo={AppNavigationLinks.simpleResume}
-              text='Resume'
-            />
-            {/* <NavigationItem navigationClass="nav-interactive-resume" linkTo="/interactive-resume" text="Immersive Resume" /> */}
-            {/* <NavigationItem navigationClass="nav-contact" linkTo="/contact-me" text="Contact Me" /> */}
-            {/* <li className='demos'>
+      <div className='hamburger' onClick={hamburgerClicked}>
+        {/* add class "W" to change the state */}
+        <div className='line line-1'></div>
+        <div className='line line-2'></div>
+        <div className='line line-3'></div>
+        <div className='line line-4'></div>
+      </div>
+      <div className='nav-tabs-container'>
+        <ul className='nav-tabs'>
+          <NavigationItem
+            navigationClass='nav-home'
+            linkTo={AppNavigationLinks.home}
+            text='Home'
+          />
+          {/* <NavigationItem navigationClass="nav-about" linkTo="/aboutwaleed" text="About me" /> */}
+          <NavigationItem
+            navigationClass='nav-simplistic-resume'
+            linkTo={AppNavigationLinks.simpleResume}
+            text='Resume'
+          />
+          {/* <NavigationItem navigationClass="nav-interactive-resume" linkTo="/interactive-resume" text="Immersive Resume" /> */}
+          {/* <NavigationItem navigationClass="nav-contact" linkTo="/contact-me" text="Contact Me" /> */}
+          {/* <li className='demos'>
               <p>Demos</p>
             </li> */}
-            <NavigationItem
-              navigationClass='demo nav-demos'
-              linkTo='/video-player'
-              text='Demos'
-            />
-            <NavigationItem
-              navigationClass='demo nav-video-player'
-              linkTo='/video-player'
-              text='Video Player'
-            />
-            <NavigationItem
-              navigationClass='demo nav-loading-animation'
-              linkTo={AppNavigationLinks.loadingAnimations}
-              text='Loaders'
-            />
-            {/* <NavigationItem navigationClass="demo nav-email-signatures" linkTo={AppNavigationLinks.EmailSignatures} text="Email Signatures" /> */}
-            {/* <NavigationItem navigationClass="demo nav-morse-code" linkTo="/morse-code" text="Morse Code" /> */}
-          </ul>
-        </div>
-      </nav>
+          <NavigationItem
+            navigationClass='demo nav-demos'
+            linkTo='/video-player'
+            text='Demos'
+          />
+          <NavigationItem
+            navigationClass='demo nav-video-player'
+            linkTo='/video-player'
+            text='Video Player'
+          />
+          <NavigationItem
+            navigationClass='demo nav-loading-animation'
+            linkTo={AppNavigationLinks.loadingAnimations}
+            text='Loaders'
+          />
+          {/* <NavigationItem navigationClass="demo nav-email-signatures" linkTo={AppNavigationLinks.EmailSignatures} text="Email Signatures" /> */}
+          {/* <NavigationItem navigationClass="demo nav-morse-code" linkTo="/morse-code" text="Morse Code" /> */}
+        </ul>
+      </div>
     </header>
   );
 }
